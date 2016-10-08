e0365cdf1dcc6798e778dd6e9478751470a600a7
Descalon <info@nicoglas.com>
2016-10-08 16:00:37 +0200

    Updated tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -12,11 +12,12 @@ import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
-
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite("#applyRules()", () =>{
+    var psd = new myExtension.PowerShellDocument();
+    test("Should rewrite the file to CamelCase all keywords",() => {
+        let input = "if";
+        let expectedOutput = "If";
+        let output = psd.applyRules(input);
+        assert.equal(output,expectedOutput);
     });
-});
+})
